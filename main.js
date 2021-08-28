d7aff8f4a18f98d55ebd5df997293af1927d2aa8
Anais Zulfequar <anaiszed447@gmail.com>
2021-08-27 23:08:02 -0400

    Created the modal when user clicks on view button

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const submit = document.querySelector('#submit')
 //accessing the note section div element
 const noteSection = document.querySelector('.view-notes')
 
+//accessing the document body
+const body = document.body
+
 //button's action when it's clicked
 submit.addEventListener('click', () =>{
     //creating a promise to check if value given is not empty
@@ -42,9 +45,7 @@ const createNote = (note) =>{
     //note's value will become the text for paragraph
     theNote.innerText = note
 
-    //creating a delete button
-    const deleteButton = document.createElement('button')
-    deleteButton.innerText = 'DELETE'
+
 
     //creating the view button
     const viewButton = document.createElement('button')
@@ -52,11 +53,12 @@ const createNote = (note) =>{
 
     //appending paragraph into div
     divNote.append(theNote)
-    divNote.append(deleteButton)
     divNote.append(viewButton)
 
     //creating a blue border for the div
     divNote.style.border = "2px solid blue"
+
+    viewNote(divNote)
 
     //returning the div
     return divNote
@@ -66,3 +68,32 @@ const createNote = (note) =>{
 const addNote = (note) => {
     noteSection.append(note)
 }
+
+const viewNote = (div) =>{
+
+    let viewsButton = div.querySelector('button')
+
+    let divParagraph = div.querySelector('p')
+    
+        viewsButton.addEventListener('click', () => {
+            theModal(divParagraph.innerText)
+            console.log('ww')
+        })
+    
+
+}
+
+const theModal = (noteText) => {
+    //creating the main div for the modal
+    const modal = document.createElement('div')
+    modal.setAttribute('style', 'position: fixed; top: 0; left: 0; height: 100%; width: 100%; background-color:  rgba(0,0,0,.5); display: flex; justify-content: center; align-items: center;')
+    
+    //creating a div to place the text of the note
+    const modalWindow = document.createElement('div')
+    modalWindow.innerText = noteText
+    modalWindow.setAttribute('style', 'position: relative; background-color: white; padding: 4em 2em;')
+
+    modal.append(modalWindow)
+    body.append(modal)
+}
+
